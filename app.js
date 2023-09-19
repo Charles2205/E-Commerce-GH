c3073dd3b672ef42eb46f41edab1e5ac00f0efc7
oseijoel6111 <oseijoel6111@gmail.com>
2023-09-19 09:11:35 -0400

    update:setting view layout with static files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotENV = require("dotenv");
 const { adminRoute, frontendRoute } = require("./routes");
+const expressEjsLayout = require('express-ejs-layouts')
 
 // configure .env
 dotENV.config();
@@ -8,6 +9,14 @@ dotENV.config();
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
+
+// serve static files
+app.use(express.static(__dirname+'/public'))
+
+// setting view engine and layouts
+app.set('view', 'ejs')
+app.use(expressEjsLayout())
+
 
 // Users routes
 app.use("/", frontendRoute);
