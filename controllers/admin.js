71908d9e45404b8801adfd5757fa9d3fa0a87c69
oseijoel6111 <oseijoel6111@gmail.com>
2023-09-20 09:16:09 -0400

    update:Admin login panel integrated

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,8 +3,11 @@ const index = (req, res)=>{
     res.render('pages/admin/pages/index')
 }
 
-
+const viewLogin = (req, res)=>{
+     res.render('pages/admin/pages/login', {layout: 'pages/admin/layouts/other'})
+}
 
 module.exports = {
-    index
+    index,
+    viewLogin
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const {adminController} = require('../controllers')
 const app = express.Router()
 
 app.get('/', adminController.index)
+app.get('/login', adminController.viewLogin)
 
 
 module.exports = app
